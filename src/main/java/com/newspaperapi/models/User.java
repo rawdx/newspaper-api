78e47eae3ddaed973f617c8d5ecf1a4fac071fdc
DESKTOP-KNBB5Q9\raulc <xxrawd@gmail.com>
2024-01-18 22:41:02 +0100

    no message

--- src/main/java/com/newspaperapi/models/User.java
+++ src/main/java/com/newspaperapi/models/User.java
@@ -28,7 +28,7 @@ public class User {
 	@Column(name = "name", length = 50)
 	private String name;
 
-	@Column(name = "phone_number")
+	@Column(name = "phone_number", length = 15)
 	private String phoneNumber;
 
 	@Column(name = "profile_picture")
@@ -46,4 +46,108 @@ public class User {
 	@OneToMany(mappedBy = "user")
 	private List<Article> articles;
 
+	
+    public User() {
+    }
+
+
+	public long getIdUser() {
+		return idUser;
+	}
+
+
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getCredential() {
+		return credential;
+	}
+
+
+	public void setCredential(String credential) {
+		this.credential = credential;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public byte[] getProfilePicture() {
+		return profilePicture;
+	}
+
+
+	public void setProfilePicture(byte[] profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+
+	public boolean isSuscribed() {
+		return isSuscribed;
+	}
+
+
+	public void setSuscribed(boolean isSuscribed) {
+		this.isSuscribed = isSuscribed;
+	}
+
+
+	public boolean isWriter() {
+		return isWriter;
+	}
+
+
+	public void setWriter(boolean isWriter) {
+		this.isWriter = isWriter;
+	}
+
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+    
 }
